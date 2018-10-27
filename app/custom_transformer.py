89d1b01b0c2084696bb4f5efe4da677f08e04fe0
shobhit upadhyaya <sho.upadhyaya@gmail.com>
2018-10-27 15:30:21 +0530

    Updated customer_transformer

--- app/custom_transformer.py
+++ app/custom_transformer.py
@@ -2,42 +2,6 @@ from sklearn.base import BaseEstimator, TransformerMixin
 import pandas as pd
 
 
-class PosTagMatrix(BaseEstimator, TransformerMixin):
-    #normalise = True - devide all values by a total number of tags in the sentence
-    #tokenizer - take a custom tokenizer function
-    def __init__(self, tokenizer=lambda x: x.split(), normalize=True):
-        self.tokenizer=tokenizer
-        self.normalize=normalize
-
-    #helper function to tokenize and count parts of speech
-    def pos_func(self, sentence):
-        return Counter(tag for word,tag in nltk.pos_tag(self.tokenizer(sentence)))
-
-    # fit() doesn't do anything, this is a transformer class
-    def fit(self, X, y = None):
-        return self
-
-    #all the work is done here
-    def transform(self, X):
-        X_tagged = X.apply(self.pos_func).apply(pd.Series).fillna(0)
-        X_tagged['n_tokens'] = X_tagged.apply(sum, axis=1)
-        if self.normalize:
-            X_tagged = X_tagged.divide(X_tagged['n_tokens'], axis=0)
-
-        return X_tagged.fillna(0.0).values
-
-
-
-class FeatureExtractor(BaseEstimator, TransformerMixin):
-    def __init__(self, feature_name):
-        self._feature_name = feature_name
-        
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
-        return X[self._feature_name].values.reshape(-1,1)
-
 
 class MessageExtractor(BaseEstimator, TransformerMixin):
     
@@ -46,6 +10,7 @@ class MessageExtractor(BaseEstimator, TransformerMixin):
 
     def transform(self, X):
         return X["message"]#.values.reshape(-1,1)
+
 
 class GenreExtractor(BaseEstimator, TransformerMixin):
     
